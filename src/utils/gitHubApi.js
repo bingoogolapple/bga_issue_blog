4c22599338ce91c3dac28ffd245dc78e07d305a9
bingoogolapple <bingoogolapple@gmail.com>
2017-07-19 20:03:04 +0800

    「update」仅加载 open 状态的 Issue

--- src/utils/gitHubApi.js
+++ src/utils/gitHubApi.js
@@ -18,7 +18,7 @@ module.exports = {
       label = `+label:${data.label}`
     }
 
-    return vue.$http.get(`https://api.github.com/search/issues?q=${data.keyword}+repo:${vue.$store.getters.context}${label}&sort=created&order=desc&page=${data.currentPage}&per_page=${data.pageSize}`)
+    return vue.$http.get(`https://api.github.com/search/issues?q=${data.keyword}+state:open+repo:${vue.$store.getters.context}${label}&sort=created&order=desc&page=${data.currentPage}&per_page=${data.pageSize}`)
   },
   getIssue (vue, number) {
     return vue.$http.get(`https://api.github.com/repos/${vue.$store.getters.context}/issues/${number}`)
