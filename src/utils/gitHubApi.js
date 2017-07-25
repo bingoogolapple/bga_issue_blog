2992ba28ada89145a40156e210e14390cd191e46
bingoogolapple <bingoogolapple@gmail.com>
2017-07-25 17:40:07 +0800

    「add」支持非 xxx.github.io 项目

--- src/utils/gitHubApi.js
+++ src/utils/gitHubApi.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   getLabels (vue) {
-    return vue.$http.get(`https://api.github.com/repos/${vue.$store.getters.context}/labels`)
+    return vue.$http.get(`https://api.github.com/repos/${vue.$store.getters.repo}/labels`)
   },
   getGitHubUser (vue) {
     return vue.$http.get(`https://api.github.com/users/${vue.$store.getters.gitHubUsername}`)
@@ -18,15 +18,15 @@ module.exports = {
       label = `+label:${data.label}`
     }
 
-    return vue.$http.get(`https://api.github.com/search/issues?q=${data.keyword}+state:open+repo:${vue.$store.getters.context}${label}&sort=created&order=desc&page=${data.currentPage}&per_page=${data.pageSize}`)
+    return vue.$http.get(`https://api.github.com/search/issues?q=${data.keyword}+state:open+repo:${vue.$store.getters.repo}${label}&sort=created&order=desc&page=${data.currentPage}&per_page=${data.pageSize}`)
   },
   getIssue (vue, number) {
-    return vue.$http.get(`https://api.github.com/repos/${vue.$store.getters.context}/issues/${number}`)
+    return vue.$http.get(`https://api.github.com/repos/${vue.$store.getters.repo}/issues/${number}`)
   },
   getComments (vue, url) {
     return vue.$http.get(url)
   },
   getReadme (vue) {
-    return vue.$http.get(`https://raw.githubusercontent.com/${vue.$store.getters.context}/master/README.md`)
+    return vue.$http.get(`https://raw.githubusercontent.com/${vue.$store.getters.repo}/master/README.md`)
   }
 }
