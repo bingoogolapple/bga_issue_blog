13bbded442cf2bfaa726a17a119ca65c928dbe00
bingoogolapple <bingoogolapple@gmail.com>
2017-08-19 11:23:22 +0800

    「update」修改 MarkDown 渲染，修改点击顶部标签跳转逻辑

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -7,7 +7,7 @@
       </div>
       <span class="time">{{$moment(comment.created_at).format('YYYY-MM-DD HH:mm')}}</span>
     </div>
-    <article class="comment-body markdown-body" v-html="comment.body_html"/>
+    <article class="comment-body markdown-body" v-html="renderedMarkdown"/>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -54,11 +54,29 @@
 </style>
 <script>
   export default {
+    data () {
+      return {
+        renderedMarkdown: ''
+      }
+    },
     props: ['comment'],
     methods: {
+      renderMarkdown () {
+        this.renderedMarkdown = ''
+        if (this.comment.body_html) {
+          this.renderedMarkdown = this.comment.body_html
+        } else if (this.comment.body) {
+          this.renderedMarkdown = this.$marked(this.comment.body)
+        }
+      },
       openGitHub () {
         window.open(this.comment.user.html_url)
       }
+    },
+    mounted () {
+      this.$nextTick(() => {
+        this.renderMarkdown()
+      })
     }
   }
 </script>
