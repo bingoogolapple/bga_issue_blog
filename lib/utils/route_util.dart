7ed8c35fb5d27791e2c1f7e2dc272cd024663101
bingoogolapple <bingoogolapple@gmail.com>
2019-10-19 21:26:15 +0800

    使用 CupertinoPageRoute 支持在 Android 设备上滑动返回

--- lib/utils/route_util.dart
+++ lib/utils/route_util.dart
@@ -1,15 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 abstract class RouteUtil {
   static routeWithFadeTransition(BuildContext context, Widget page) {
-    Navigator.of(context).push(PageRouteBuilder(
-        opaque: false,
-        pageBuilder: (BuildContext context, _, __) {
-          return page;
-        },
-        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
-          return _transition(animation, child);
-        }));
+    // 使用 CupertinoPageRoute 支持滑动返回
+    Navigator.of(context).push(CupertinoPageRoute(builder: (context) {
+      return page;
+    }));
+//    Navigator.of(context).push(PageRouteBuilder(
+//        opaque: false,
+//        pageBuilder: (BuildContext context, _, __) {
+//          return page;
+//        },
+//        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
+//          return _transition(animation, child);
+//        }));
   }
 
   static FadeTransition _transition(Animation<double> animation, Widget child) {
