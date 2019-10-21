3b67c542693eeeedb3cf3cd243bf16c1d099791b
bingoogolapple <bingoogolapple@gmail.com>
2019-10-21 21:49:35 +0800

    Android、iOS 支持选择复制，Web 还不支持

--- lib/widget/issue_item.dart
+++ lib/widget/issue_item.dart
@@ -1,5 +1,6 @@
 import 'package:bga_issue_blog/page/blog_detail_page.dart';
 import 'package:bga_issue_blog/utils/route_util.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class IssueItem extends StatelessWidget {
@@ -11,16 +12,30 @@ class IssueItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
-        title: GestureDetector(
-          child: Text(issue['title']),
-//          onTap: () => RouteUtil.pushWithSwipeBackTransition(context, BlogDetailPage(number: issue['number'])),
-          onTap: () => RouteUtil.routeToBlogDetail(context, issue['number']),
-//          onTap: () => Navigator.pushNamed(context, '/blog', arguments: issue['number']),
-        ),
+        title: _buildTitle(context),
         trailing: Text(DateTime.tryParse(issue['created_at']).toString().substring(0, "yyyy-MM-dd HH:mm".length)),
       ),
       decoration: BoxDecoration(
         border: Border(bottom: BorderSide(color: Color.fromARGB(255, 238, 238, 238))),
+      ),
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    // Web 还不支持选择复制，Android、iOS 可以，但点击空白区域时，拷贝、全选浮窗不会自动消失
+    if (kIsWeb) {
+      return GestureDetector(
+        child: Text(issue['title']),
+//        onTap: () => RouteUtil.pushWithSwipeBackTransition(context, BlogDetailPage(number: issue['number'])),
+//        onTap: () => Navigator.pushNamed(context, '/blog', arguments: issue['number']),
+        onTap: () => RouteUtil.routeToBlogDetail(context, issue['number']),
+      );
+    }
+    return GestureDetector(
+      child: SelectableText(
+        issue['title'],
+        onTap: () => RouteUtil.routeToBlogDetail(context, issue['number']),
+        toolbarOptions: ToolbarOptions(copy: true, selectAll: true),
       ),
     );
   }
